c786e77ea510e59126d004633fa2e14a00d42961
Okan <okanaydinhan981@gmail.com>
2025-02-25 17:45:21 +0300

    odev
    C# dili ile OOP kavramlarından kalıtım(inheritance) kavramını öğrendim

---
+++ odev/odev/Araba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev
+{
+    public class Araba
+    {
+        public string marka = null;
+        public string vites = null;
+    
+    
+        public void markaVitesYaz(string marka,string vites)
+        {
+            Console.WriteLine(marka + " " + vites+"lidir.");
+        }
+    }
+}
---
+++ odev/odev/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using odev;
+
+
+Bmw bm = new Bmw();
+
+bm.marka = "Bmw";
+bm.vites = "Düz vites";
+
+
+Porsche po = new Porsche();
+
+po.marka = "Porsche";
+po.vites = "Otomatik vites";
+
+
+Mercedes me = new Mercedes();
+
+me.marka = "Mercedes";
+me.vites = "Düz vites";
+
+
+Audi au = new Audi();
+
+au.marka = "Audi";
+au.vites = "Otomatik vites";
+
+
+Togg to = new Togg();
+
+to.marka = "Togg";
+to.vites = "Otomatik vites";
+
+
+Toyota ty = new Toyota();
+
+ty.marka = "Toyota";
+ty.vites = "Düz vites";
+
+
+bm.markaVitesYaz(bm.marka, bm.vites);
+po.markaVitesYaz(po.marka, po.vites);
+me.markaVitesYaz(me.marka, me.vites);
+to.markaVitesYaz(to.marka, to.vites);
+au.markaVitesYaz(au.marka, au.vites);
+ty.markaVitesYaz(ty.marka, ty.vites);
